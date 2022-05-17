8741e33c8094220964fbcbc3f1df051c111eb5f8
carosteguig <carosteguig@gmail.com>
2022-05-17 14:48:44 -0700

    set up index.js with install, port, middleware, home route, routing and listening port

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
-require('dotenv').config();
-
-const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT;
+const cors = require('cors');
+require('dotenv').config();// load .env variables
+const paintsStock = require('./routes/paintsRoute');
+const { application } = require('express');
+
+const PORT = process.env.PORT ||  5000;
 
 //middleware
-app.use(express.json());
-// app.use(express.static('public'));
-app.use(cors());
-// app.use('/inventory', inventoryRoutes);
+app.use(express.json());  // adds required body (name: req.body.name) 
+// app.use(express.static('public'));  // allows to serve the images in public folder.
+app.use(cors()); // allow cross origin resource sharing
+
+
+// home route
+app.use('/', (req, res) => {
+    res.send('Welcome to server API')
+});
+
+//routing
+app.use('/paints', paintsStock);
+
+app.listen(PORT, () => {
+    console.log(`Hello! My server is listening on port ${PORT}`)
+});
